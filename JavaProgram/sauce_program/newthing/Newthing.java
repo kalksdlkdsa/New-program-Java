25f6440af1cbb879d8635d987fe6916ecd29e068
RatRatARMY <156580271+kalksdlkdsa@users.noreply.github.com>
2024-04-16 18:51:32 +0700

    Create Newthing.java

---
+++ JavaProgram/sauce_program/newthing/Newthing.java
@@ -0,0 +1,12 @@
+package newthing;
+import java.time.LocalDateTime;
+public class Newthing {
+	public static void main(String[] args) {
+		System.out.println(LocalDateTime.now());
+		System.out.println(LocalDateTime.of(+999_999_999,12,31,23,59,59,999_999_999));
+		System.out.println(LocalDateTime.of(0000,01,01,00,00,00,000_000_000));
+		System.out.println(LocalDateTime.of(-999_999_999,01,01,00,00,00,000_000_000));
+		//System.out.println(LocalDateTime.of(+1_000_000_000,01,01,00,00,00,000_000_000));
+		//System.out.println(LocalDateTime.of(-1_000_000_000,12,31,23,59,59,999_999_999));
+	}
+}
